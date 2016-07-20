5dad930333ba4384c7ac3d0710ca1349a2b4e696
Abtin Keshavarzian <abtink@nestlabs.com>
2016-07-20 16:24:29 -0700

    SpinelNCPControlInterface: Fix config_gateway, add/remove external route
    This commit changes how the `config_gateway`, `add/remove_external_route`
    are implemented in `SpinelNCPControlInterface` (spinel plugin). It ensures
    that after an update to any local network data (mesh prefixes or external
    routes), the new data is registered with the leader. This is done by adding a
    new Spinel property `THREAD_ALLOW_LOCAL_NET_DATA_CHANGE` with boolean type.
    This property is set to 'true' before modifying any properties related to
    local network data. When done, this property is set to 'false' causing the
    network data to be registered with leader. This commit also adds a new
    SpinelNCPTask, namely `SpinelNCPTaskChangeNetData` which implements the
    common logic for changing any net data property.
    
    This commit is related to openthread issue #272.

---
+++ src/ncp-spinel/SpinelNCPTaskChangeNetData.h
@@ -0,0 +1,54 @@
+/*
+ *
+ * Copyright (c) 2016 Nest Labs, Inc.
+ * All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#ifndef __wpantund__SpinelNCPTaskChangeNetData__
+#define __wpantund__SpinelNCPTaskChangeNetData__
+
+#include "SpinelNCPTask.h"
+#include "SpinelNCPInstance.h"
+#include "ValueMap.h"
+
+using namespace nl;
+using namespace nl::wpantund;
+
+namespace nl {
+namespace wpantund {
+
+class SpinelNCPTaskChangeNetData : public SpinelNCPTask
+{
+public:
+	SpinelNCPTaskChangeNetData(
+		SpinelNCPInstance* instance,
+		CallbackWithStatusArg1 cb,
+		const Data& change_net_data_command,
+		int timeout = NCP_DEFAULT_COMMAND_RESPONSE_TIMEOUT
+	);
+
+	virtual int vprocess_event(int event, va_list args);
+
+private:
+	Data mChangeNetDataCommand;
+	int mRetVal;
+};
+
+}; // namespace wpantund
+}; // namespace nl
+
+
+#endif /* defined(__wpantund__SpinelNCPTaskChangeNetData__) */
