7c8af7d26b5fe78543ea491ffd8c65c984f2b055
Abtin Keshavarzian <abtink@google.com>
2017-08-29 14:15:37 -0700

    Add new wpantund property "Thread:RouterRole:Enabled"
    This property is tied to `SPINEL_PROP_THREAD_ROUTER_ROLE_ENABLED`
    spinel property.

--- src/wpantund/wpan-properties.h
+++ src/wpantund/wpan-properties.h
@@ -107,6 +107,7 @@
 #define kWPANTUNDProperty_ThreadDeviceMode                      "Thread:DeviceMode"
 #define kWPANTUNDProperty_ThreadOffMeshRoutes                   "Thread:OffMeshRoutes"
 #define kWPANTUNDProperty_ThreadOnMeshPrefixes                  "Thread:OnMeshPrefixes"
+#define kWPANTUNDProperty_ThreadRouterRoleEnabled               "Thread:RouterRole:Enabled"
 #define kWPANTUNDProperty_ThreadConfigFilterRLOCAddresses       "Thread:Config:FilterRLOCAddresses"
 
 #define kWPANTUNDProperty_OpenThreadLogLevel                    "OpenThread:LogLevel"
