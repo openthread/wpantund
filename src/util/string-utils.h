9840a927811361bae95be379b2d3eaa3159a7ded
Anton <anton@corp.bluecherry.net>
2018-02-11 17:50:12 +0400

    Include <string.h> explicitly in string utils header. Fixes build with uclibc.

--- src/util/string-utils.h
+++ src/util/string-utils.h
@@ -28,6 +28,7 @@
 #include <stdint.h>
 #include <stdbool.h>
 #include <sys/cdefs.h>
+#include <string.h>
 
 #define strcaseequal(x, y)   (strcasecmp(x, y) == 0)
 #define strncaseequal(x, y, n)   (strncasecmp(x, y, n) == 0)
