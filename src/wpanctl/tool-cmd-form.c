e1927db9b51d4883e85c17e8c671d9490231a826
Abtin Keshavarzian <abtink@google.com>
2018-08-08 09:36:32 -0700

    Update wpanctl `form` and `join` implementation to use DBus v1 APIs
    This commit enhances the implementation of `form` and `join` wpanctl
    commands to use wpantund DBus v1 APIs (instead of v0). New (v1) APIs
    are more flexible and allow a dictionary of property key (as string),
    property value (as DBus variant type) to be provided as input. Both
    `form` and `join` wpanctl commands are updated to allow more
    (optional) parameters to be specified: `form` allows panid, xpaind,
    network key, key index, mesh-local-prefix channel mask to be
    optionally provided as part of the `form` command in addition to
    network name and/or channel. `join` allows network key to be specified
    as part of `join` command itself.

--- src/wpanctl/tool-cmd-form.c
+++ src/wpanctl/tool-cmd-form.c
@@ -25,40 +25,61 @@
 #include "wpanctl-utils.h"
 #include "tool-cmd-form.h"
 #include "assert-macros.h"
-#include "wpan-dbus-v0.h"
+#include "wpan-dbus-v1.h"
 #include "args.h"
 #include "string-utils.h"
 
 #include <arpa/inet.h>
 #include <errno.h>
 
-const char form_cmd_syntax[] = "[args] [network-name]";
+const char form_cmd_syntax[] = "[args] <network-name>";
 
 static const arg_list_item_t form_option_list[] = {
 	{'h', "help", NULL, "Print Help"},
 	{'t', "timeout", "ms", "Set timeout period"},
 	{'c', "channel", "channel", "Set the desired channel"},
+	{'m', "channel-mask", "mask", "Specify a channel mask (channel will be chosen randomly from given mask)"},
+	{'p', "panid", "panid", "Specify a specific PAN ID"},
+	{'x', "xpanid", "xpanid", "Specify a specific Extended PAN ID"},
+	{'k', "key", "key", "Specify the network key"},
+	{'i', "key-index", "index", "Specify network key index"},
 	{'T', "type", "node-type: router(r,2), end-device(end,e,3), sleepy-end-device(sleepy,sed,4), nl-lurker(lurker,l,6)",
-		"Join as a specific node type" },
-//	{'u', "ula-prefix", "ULA Prefix", "Specify a specific *LEGACY* ULA prefix"},
+		"Form as a specific node type" },
 	{'M', "mesh-local-prefix", "Mesh-Local IPv6 Prefix", "Specify a non-default mesh-local IPv6 prefix"},
 	{'L', "legacy-prefix", "Legacy IPv6 Prefix", "Specify a specific *LEGACY* IPv6 prefix"},
 	{0}
 };
 
-int tool_cmd_form(int argc, char* argv[])
+int tool_cmd_form(int argc, char *argv[])
 {
 	int ret = 0;
-	int c;
 	int timeout = DEFAULT_TIMEOUT_IN_SECONDS * 1000;
 	DBusConnection* connection = NULL;
 	DBusMessage *message = NULL;
 	DBusMessage *reply = NULL;
 	DBusError error;
-	const char* network_name = NULL;
-	const char* ula_prefix = NULL;
-	uint16_t node_type = WPAN_IFACE_ROLE_ROUTER; // Default to router for form
+	DBusMessageIter msg_iter;
+	DBusMessageIter dict_iter;
+
+	const char *network_name = NULL;
+	uint16_t channel = 0;
 	uint32_t channel_mask = 0;
+	uint16_t panid = 0;
+	uint64_t xpanid = 0;
+	uint8_t network_key[WPANCTL_NETWORK_KEY_SIZE];
+	uint32_t key_index = 0;
+	const char *node_type = kWPANTUNDNodeType_Router;
+	uint8_t mesh_local_prefix[WPANCTL_PREFIX_SIZE];
+	uint8_t legacy_prefix[WPANCTL_PREFIX_SIZE];
+	bool has_channel = false;
+	bool has_channel_mask = false;
+	bool has_panid = false;
+	bool has_xpanid = false;
+	bool has_network_key = false;
+	bool has_key_index = false;
+	bool has_node_type = false;
+	bool has_mesh_local_prefix = false;
+	bool has_legacy_prefix = false;
 
 	dbus_error_init(&error);
 
@@ -67,6 +88,11 @@ int tool_cmd_form(int argc, char* argv[])
 			{"help", no_argument, 0, 'h'},
 			{"timeout", required_argument, 0, 't'},
 			{"channel", required_argument, 0, 'c'},
+			{"channel-mask", required_argument, 0, 'm'},
+			{"panid", required_argument, 0, 'p'},
+			{"xpanid", required_argument, 0, 'x'},
+			{"key", required_argument, 0, 'k'},
+			{"key-index", required_argument, 0, 'i'},
 			{"ula-prefix", required_argument, 0, 'u'},
 			{"mesh-local-prefix", required_argument, 0, 'M'},
 			{"legacy-prefix", required_argument, 0, 'L'},
@@ -75,16 +101,17 @@ int tool_cmd_form(int argc, char* argv[])
 		};
 
 		int option_index = 0;
-		c = getopt_long(argc, argv, "hc:t:T:u:", long_options,
-				&option_index);
+		int c;
 
-		if (c == -1)
+		c = getopt_long(argc, argv, "ht:c:m:p:x:k:i:u:M:L:T:", long_options, &option_index);
+
+		if (c == -1) {
 			break;
+		}
 
 		switch (c) {
 		case 'h':
-			print_arg_list_help(form_option_list, argv[0],
-					    form_cmd_syntax);
+			print_arg_list_help(form_option_list, argv[0], form_cmd_syntax);
 			ret = ERRORCODE_HELP;
 			goto bail;
 
@@ -93,56 +120,151 @@ int tool_cmd_form(int argc, char* argv[])
 			break;
 
 		case 'c':
-			channel_mask = (1 << strtol(optarg, NULL, 0));
+			has_channel = true;
+			channel = strtol(optarg, NULL, 0);
+			break;
+
+		case 'm':
+			has_channel_mask = true;
+			channel_mask = strtomask_uint32(optarg);
+			if (channel_mask == 0) {
+				fprintf(stderr, "%s: error: Bad channel mask \"%s\"\n", argv[0], optarg);
+				ret = ERRORCODE_BADARG;
+				goto bail;
+			}
+			break;
+
+		case 'p':
+			has_panid = true;
+			panid = strtol(optarg, NULL, 0);
+			break;
+
+		case 'x':
+			has_xpanid = true;
+			xpanid = strtoull(optarg, NULL, 16);
+			break;
+
+		case 'k':
+			has_network_key = true;
+			if (parse_string_into_data(network_key, WPANCTL_NETWORK_KEY_SIZE, optarg) <= 0) {
+				fprintf(stderr, "%s: error: Bad network-key \"%s\"\n", argv[0], optarg);
+				ret = ERRORCODE_BADARG;
+				goto bail;
+			}
+			break;
+
+		case 'i':
+			has_key_index = true;
+			key_index = strtol(optarg, NULL, 0);
 			break;
 
 		case 'M':
-			fprintf(stderr,
-					"%s: error: Setting the mesh local address at the command line isn't yet implemented. Set it as a property instead.\n",
-					argv[0]);
-			ret = ERRORCODE_BADARG;
-			goto bail;
+			has_mesh_local_prefix = true;
+			ret = parse_prefix(optarg, mesh_local_prefix);
+			if (ret != ERRORCODE_OK) {
+				fprintf(stderr, "%s: error: Bad mesh-local prefix \"%s\"\n", argv[0], optarg);
+				goto bail;
+			}
 			break;
 
 		case 'L':
 		case 'u':
-			ula_prefix = optarg;
+			has_legacy_prefix = true;
+			ret = parse_prefix(optarg, legacy_prefix);
+			if (ret != ERRORCODE_OK) {
+				fprintf(stderr, "%s: error: Bad legacy prefix \"%s\"\n", argv[0], optarg);
+				goto bail;
+			}
 			break;
 
 		case 'T':
-			node_type = node_type_str2int(optarg);
+			has_node_type = true;
+			node_type = parse_node_type(optarg);
 			break;
 		}
 	}
 
 	if (optind < argc) {
-		if (!network_name) {
-			network_name = argv[optind];
-			optind++;
-		}
-	}
+		network_name = argv[optind];
+		optind++;
 
-	if (optind < argc) {
-		fprintf(stderr,
-			"%s: error: Unexpected extra argument: \"%s\"\n",
-			argv[0], argv[optind]);
+		if (strnlen(network_name, WPANCTL_NETWORK_NAME_MAX_LEN + 1) > WPANCTL_NETWORK_NAME_MAX_LEN) {
+			fprintf(stderr, "%s: error: Network name \"%s\" is too long (max %d chars)\n", argv[0], network_name,
+				WPANCTL_NETWORK_NAME_MAX_LEN);
+			ret = ERRORCODE_BADARG;
+			goto bail;
+		}
+
+	} else {
+		fprintf(stderr, "%s: error: Missing network name\n", argv[0]);
 		ret = ERRORCODE_BADARG;
 		goto bail;
 	}
 
-	if (!network_name) {
-		fprintf(stderr, "%s: error: Missing network name.\n", argv[0]);
+	if (optind < argc) {
+		fprintf(stderr, "%s: error: Unexpected extra argument: \"%s\"\n", argv[0], argv[optind]);
 		ret = ERRORCODE_BADARG;
 		goto bail;
 	}
 
 	if (gInterfaceName[0] == 0) {
 		fprintf(stderr,
-		        "%s: error: No WPAN interface set (use the `cd` command, or the `-I` argument for `wpanctl`).\n",
-		        argv[0]);
+			"%s: error: No WPAN interface set (use the `cd` command, or the `-I` argument for `wpanctl`).\n", argv[0]);
 		ret = ERRORCODE_BADARG;
 		goto bail;
 	}
+
+	// Print the parameters
+
+	fprintf(stdout, "Forming WPAN \"%s\" as node type \"%s\"", network_name, node_type);
+
+	if (has_channel) {
+		fprintf(stdout, ", channel:%d", channel);
+	} else if (has_channel_mask) {
+		fprintf(stdout, ", channel-mask:0x%x", channel_mask);
+	}
+
+	if (has_panid) {
+		fprintf(stdout, ", panid:0x%04X", panid);
+	}
+
+	if (has_xpanid) {
+		fprintf(stdout, ", xpanid:0x%016llX", (unsigned long long)xpanid);
+	}
+
+	if (has_network_key) {
+		char key_str[WPANCTL_NETWORK_KEY_SIZE * 2 + 4];
+		encode_data_into_string(network_key, sizeof(network_key), key_str, sizeof(key_str), 0);
+		fprintf(stdout, ", key:[%s]", key_str);
+	}
+
+	if (has_key_index) {
+		fprintf(stdout, ", key-index:%d", key_index);
+	}
+
+	if (has_mesh_local_prefix) {
+		char address_string[INET6_ADDRSTRLEN] = "::";
+		uint8_t prefix_address[WPANCTL_IPv6_ADDRESS_SIZE];
+
+		memset(prefix_address, 0, sizeof(prefix_address));
+		memcpy(prefix_address, mesh_local_prefix, WPANCTL_PREFIX_SIZE);
+		inet_ntop(AF_INET6, (const void *)&prefix_address, address_string, sizeof(address_string));
+		fprintf(stdout, ", mesh-local-prefix:\"%s\"", address_string);
+	}
+
+	if (has_legacy_prefix) {
+		char address_string[INET6_ADDRSTRLEN] = "::";
+		uint8_t prefix_address[WPANCTL_IPv6_ADDRESS_SIZE];
+
+		memset(prefix_address, 0, sizeof(prefix_address));
+		memcpy(prefix_address, legacy_prefix, WPANCTL_PREFIX_SIZE);
+		inet_ntop(AF_INET6, (const void *)&prefix_address, address_string, sizeof(address_string));
+		fprintf(stdout, ", legacy-prefix:\"%s\"", address_string);
+	}
+
+	fprintf(stdout, "\n");
+
+	// Prepare DBus connection
 
 	connection = dbus_bus_get(DBUS_BUS_STARTER, &error);
 
@@ -154,122 +276,145 @@ int tool_cmd_form(int argc, char* argv[])
 
 	require_string(connection != NULL, bail, error.message);
 
-	{
-		DBusMessageIter iter;
-		DBusMessageIter list_iter;
-		char path[DBUS_MAXIMUM_NAME_LENGTH+1];
-		char interface_dbus_name[DBUS_MAXIMUM_NAME_LENGTH+1];
-		ret = lookup_dbus_name_from_interface(interface_dbus_name, gInterfaceName);
-		if (ret != 0) {
-			goto bail;
-		}
-		snprintf(path,
-		         sizeof(path),
-		         "%s/%s",
-		         WPAN_TUNNEL_DBUS_PATH,
-		         gInterfaceName);
+	// Prepare DBus message
 
-		message = dbus_message_new_method_call(
-		    interface_dbus_name,
-		    path,
-		    WPAN_TUNNEL_DBUS_INTERFACE,
-		    WPAN_IFACE_CMD_FORM
-		    );
+	ret = create_new_wpan_dbus_message(&message, WPANTUND_IF_CMD_FORM);
+	require_action(ret == 0, bail, print_error_diagnosis(ret));
 
-		dbus_message_append_args(
-		    message,
-		    DBUS_TYPE_STRING, &network_name,
-		    DBUS_TYPE_INT16, &node_type,
-		    DBUS_TYPE_UINT32, &channel_mask,
-		    DBUS_TYPE_INVALID
-		    );
+	dbus_message_iter_init_append(message, &msg_iter);
 
-		if(ula_prefix) {
-			uint8_t ula_bytes[16] = {};
+	// Open a container as "Array of Dictionary entries from String to Variants" (dbus type "a{sv}")
+	dbus_message_iter_open_container(
+		&msg_iter,
+		DBUS_TYPE_ARRAY,
+		DBUS_DICT_ENTRY_BEGIN_CHAR_AS_STRING
+			DBUS_TYPE_STRING_AS_STRING
+			DBUS_TYPE_VARIANT_AS_STRING
+		DBUS_DICT_ENTRY_END_CHAR_AS_STRING,
+		&dict_iter
+	);
 
-			// So the ULA prefix could either be
-			// specified like an IPv6 address, or
-			// specified as a bunch of hex numbers.
-			// We use the presence of a colon (':')
-			// to differentiate.
-			if(strstr(ula_prefix,":")) {
+	// Append dictionary entries
 
-				// Address-style
-				int bits = inet_pton(AF_INET6,ula_prefix,ula_bytes);
-				if(bits<0) {
-					fprintf(stderr,
-					        "Bad ULA \"%s\", errno=%d (%s)\n",
-					        ula_prefix,
-					        errno,
-					        strerror(errno));
-					goto bail;
-				} else if(!bits) {
-					fprintf(stderr, "Bad ULA \"%s\"\n", ula_prefix);
-					goto bail;
-				}
-			} else {
-				// DATA-style
-				int length = parse_string_into_data(ula_bytes,
-				                                    8,
-				                                    ula_prefix);
-				if(length<=0) {
-					fprintf(stderr, "Bad ULA \"%s\"\n", ula_prefix);
-					goto bail;
-				}
-			}
+	append_dbus_dict_entry_basic(
+		&dict_iter,
+		kWPANTUNDProperty_NetworkName,
+		DBUS_TYPE_STRING, &network_name
+	);
 
-			fprintf(stderr, "Using ULA prefix \"%s\"\n", ula_prefix);
+	if (has_node_type) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NetworkNodeType,
+			DBUS_TYPE_STRING, &node_type
+		);
+	}
 
-			uint8_t *addr = ula_bytes;
-			dbus_message_append_args(
-			    message,
-			    DBUS_TYPE_ARRAY, DBUS_TYPE_BYTE, &addr, 8,
-			    DBUS_TYPE_INVALID
-			    );
-		}
+	if (has_channel) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NCPChannel,
+			DBUS_TYPE_UINT16, &channel
+		);
+	}
 
-		fprintf(stderr,
-		        "Forming WPAN \"%s\" as node type \"%s\"\n",
-		        network_name,
-		        node_type_int2str(node_type));
+	if (has_channel_mask) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NCPChannelMask,
+			DBUS_TYPE_UINT32, &channel_mask
+		);
+	}
 
+	if (has_panid) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NetworkPANID,
+			DBUS_TYPE_UINT16, &panid
+		);
+	}
 
-		reply = dbus_connection_send_with_reply_and_block(
-		    connection,
-		    message,
-		    timeout,
-		    &error
-		    );
+	if (has_xpanid) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NetworkXPANID,
+			DBUS_TYPE_UINT64, &xpanid
+		);
+	}
 
-		if (!reply) {
-			fprintf(stderr, "%s: error: %s\n", argv[0], error.message);
-			ret = ERRORCODE_TIMEOUT;
-			goto bail;
-		}
+	if (has_network_key) {
+		append_dbus_dict_entry_byte_array(
+			&dict_iter,
+			kWPANTUNDProperty_NetworkKey,
+			network_key,
+			sizeof(network_key)
+		);
+	}
 
-		dbus_message_get_args(reply, &error,
-		                      DBUS_TYPE_INT32, &ret,
-		                      DBUS_TYPE_INVALID
-		                      );
+	if (has_key_index) {
+		append_dbus_dict_entry_basic(
+			&dict_iter,
+			kWPANTUNDProperty_NetworkKeyIndex,
+			DBUS_TYPE_UINT32, &key_index
+		);
+	}
 
-		if (!ret) {
-			fprintf(stderr, "Successfully formed!\n");
-		} else {
-			fprintf(stderr, "%s failed with error %d. %s\n", argv[0], ret, wpantund_status_to_cstr(ret));
-			print_error_diagnosis(ret);
-		}
+	if (has_mesh_local_prefix) {
+		append_dbus_dict_entry_byte_array(
+			&dict_iter,
+			kWPANTUNDProperty_IPv6MeshLocalPrefix,
+			mesh_local_prefix,
+			WPANCTL_PREFIX_SIZE
+		);
+	}
+
+	if (has_legacy_prefix) {
+		append_dbus_dict_entry_byte_array(
+			&dict_iter,
+			kWPANTUNDProperty_NestLabs_LegacyMeshLocalPrefix,
+			legacy_prefix,
+			WPANCTL_PREFIX_SIZE
+		);
+	}
+
+	dbus_message_iter_close_container(&msg_iter, &dict_iter);
+
+	// Send DBus message and parse the DBus reply
+
+	reply = dbus_connection_send_with_reply_and_block(connection, message, timeout, &error);
+
+	if (!reply) {
+		fprintf(stderr, "%s: error: %s\n", argv[0], error.message);
+		ret = ERRORCODE_TIMEOUT;
+		goto bail;
+	}
+
+	dbus_message_get_args(
+		reply, &error,
+		DBUS_TYPE_INT32, &ret,
+		DBUS_TYPE_INVALID
+	);
+
+	if (!ret) {
+		fprintf(stdout, "Successfully formed!\n");
+	} else {
+		fprintf(stderr, "%s failed with error %d. %s\n", argv[0], ret, wpantund_status_to_cstr(ret));
+		print_error_diagnosis(ret);
 	}
 
 bail:
 
-	if (connection)
+	if (connection) {
 		dbus_connection_unref(connection);
+	}
 
-	if (message)
+	if (message) {
 		dbus_message_unref(message);
+	}
 
-	if (reply)
+	if (reply) {
 		dbus_message_unref(reply);
+	}
 
 	dbus_error_free(&error);
 
