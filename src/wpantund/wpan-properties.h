32b29ce602bc8df4b20b9c93ad9866ad00fc3ffd
Abtin Keshavarzian <abtink@google.com>
2019-10-09 13:43:58 -0700

    Feature to auto add on-mesh prefixes as interface route (#438)
    This commit adds a new feature in `wpantund` to auto add a route on
    the host primary interface corresponding to on-mesh prefixes from
    Thread network.
    
    This feature can be enabled through a newly added wpan property with
    name "Daemon:OnMeshPrefix:AutoAddAsIntterfaceRoute" which is enabled
    by default.
    
    When enabled, wpantund would add a route on host primary interface for
    any prefix from thread network (with on-mesh flag set). This in turn
    ensures that traffic destined to an IPv6 address matching the prefix
    would be correctly forwarded to the `wpan` interface on host.

--- src/wpantund/wpan-properties.h
+++ src/wpantund/wpan-properties.h
@@ -55,6 +55,7 @@
 #define kWPANTUNDProperty_DaemonSetDefRouteForAutoAddedPrefix   "Daemon:SetDefaultRouteForAutoAddedPrefix"
 #define kWPANTUNDProperty_DaemonOffMeshRouteAutoAddOnInterface  "Daemon:OffMeshRoute:AutoAddOnInterface"
 #define kWPANTUNDProperty_DaemonOffMeshRouteFilterSelfAutoAdded "Daemon:OffMeshRoute:FilterSelfAutoAdded"
+#define kWPANTUNDProperty_DaemonOnMeshPrefixAutoAddAsIfaceRoute "Daemon:OnMeshPrefix:AutoAddAsInterfaceRoute"
 
 #define kWPANTUNDProperty_NCPVersion                            "NCP:Version"
 #define kWPANTUNDProperty_NCPState                              "NCP:State"
